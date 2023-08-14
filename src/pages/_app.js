c83fb7ab25071a438cc786398600f7431731ce32
denniswebdel <denniswebdel@gmail.com>
2023-08-14 19:55:05 +0100

    feat (config): add storyblok dynamic story fetching and configure nextjs ISR

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,59 @@
-import '@/styles/globals.css'
+import { storyblokInit, apiPlugin } from '@storyblok/react'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import ContentSection1 from '@/components/ContentSection1'
+import ContentSection2 from '@/components/ContentSection2'
+import FAQSection from '@/components/FAQSection'
+import FeaturedServices from '@/components/FeaturedServices'
+import Features from '@/components/Features'
+import GoogleMapsIFrame from '@/components/GoogleMapsIFrame'
+import Hero from '@/components/Hero'
+import Layout from '@/components/Layout'
+import MenuLink from '@/components/MenuLink'
+import Page from '@/components/Page'
+import PricingSection from '@/components/PricingSection'
+import ReviewsSection from '@/components/ReviewsSection'
+import Spacer from '@/components/Spacer'
+import theme from '@/theme'
+import ConfigContext from '@/context/config'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const components = {
+  ContentSection1,
+  ContentSection2,
+  FAQSection,
+  FeaturedServices,
+  Features,
+  GoogleMapsIFrame,
+  Hero,
+  MenuLink,
+  Page,
+  PricingSection,
+  ReviewsSection,
+  Spacer
 }
+
+// initialize the storyblok SDK
+storyblokInit({
+  accessToken: process.env.storyblokPreviewToken,
+  use: [apiPlugin],
+  components
+})
+
+/**
+ * The function `MyApp` is a React component that wraps the provided `Component` with a config provider
+ * theme provider, CSS baseline, and a layout component.
+ */
+function MyApp ({ Component, pageProps }) {
+  return (
+    <ConfigContext.Provider value={pageProps.config}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </ConfigContext.Provider>
+  )
+}
+
+export default MyApp
