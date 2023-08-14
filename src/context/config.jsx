c5e30f69d995fa5a92be6ab444768d3dd8183e4e
denniswebdel <denniswebdel@gmail.com>
2023-08-14 19:55:46 +0100

    feat (config): add storyblok general config story to react context

---
+++ src/context/config.jsx
@@ -0,0 +1,6 @@
+import { createContext } from 'react'
+
+/** This is the context holding the config story data from storyblok(business address, navigation links, footer content ...)  */
+const configContext = createContext(null)
+
+export default configContext
